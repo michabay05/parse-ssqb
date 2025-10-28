0221f537205a9c92bd2b690b1bd63c62549a144f
michabay05 <michabay05@gmail.com>
2025-10-28 07:40:32 -0400

    exporting to json and csv & handle multi-page questions

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import datetime as dt
 import json, re, time
 from typing import Literal
 
+import pandas as pd
 import fitz
+from pymupdf import Document
 
 # TODOs
-#   - [ ] Some questions take up more than one page. Identify a mechanism to identify the page ranges of a question
+#   - [x] Some questions take up more than one page. Identify a mechanism to identify the page ranges of a question
+#   - [ ] Fix duplications of the same questions
+#   - [x] Fix parsing issue with R&W pdfs
+#   - [ ] Output to both json and csv
+#       - Use pandas dataframe
+#   - [ ] Add repl to interact with and has the following features
+#       - [ ] Filtering
+#       - [ ] Question organizations (r&w hard random, math easy random, math hard 1,2,4,23)
+#       - [ ] Any combination of questions
 
 # Code used to select all checkboxes
 # ======================================================
@@ -24,88 +34,172 @@ import fitz
 
 Level = Literal["easy","medium", "hard"]
 
+class Timer:
+    def __init__(self) -> None:
+        self._start: float = time.time()
+
+    def start(self) -> None:
+        self._start: float = time.time()
+
+    def stop(self, msg: str) -> None:
+        diff = time.time() - self._start
+        if diff >= 1.0:
+            diff_str = f"{diff:.3f} s"
+        else:
+            diff_ms = diff * 1000
+            diff_str = f"{diff_ms:.3f} ms"
+
+        print(f"[{diff_str:>10}] {msg}")
+
+
+def pdf_output_path_name(subject: str, difficulty: str, excluded: bool) -> str:
+    excluded_str = "excluded" if excluded else ""
+
+    return f"{subject}-{excluded_str}-{difficulty}"
+
+
+pdf_paths: dict[str, dict] = {
+    "all-math-excluded-easy.pdf": {
+        "subject": "Math",
+        "difficulty": "easy",
+        "excluded": True,
+    },
+    "all-math-excluded-medium.pdf": {
+        "subject": "Math",
+        "difficulty": "medium",
+        "excluded": True,
+    },
+    "all-math-excluded-hard.pdf": {
+        "subject": "Math",
+        "difficulty": "hard",
+        "excluded": True,
+    },
+    "all-rw-excluded-easy.pdf": {
+        "subject": "R&W",
+        "difficulty": "easy",
+        "excluded": True,
+    },
+    "all-rw-excluded-medium.pdf": {
+        "subject": "R&W",
+        "difficulty": "medium",
+        "excluded": True,
+    },
+    "all-rw-excluded-hard.pdf": {
+        "subject": "R&W",
+        "difficulty": "hard",
+        "excluded": True,
+    },
+}
+
 @dataclass
 class SSQBInfo:
     q_id: str
-    assessment: Literal['SAT']
-    test: str
     domain: str
     skill: str
-    difficulty: Level
-    page_no: int
+    page_nos: str
+
+timer: Timer = Timer()
 
 
-def identify_difficulty(filepath: str) -> Level | None:
-    is_easy = filepath.find(filepath, "easy") >= 0
-    is_medium = filepath.find(filepath, "medium") >= 0
-    is_hard = filepath.find(filepath, "hard") >= 0
+def process_question_pdf(path: str, output_path_name: str, format: Literal['csv', 'json']) -> None:
+    global timer
 
-    assert is_easy + is_medium + is_hard == 2,
-        "The file name should have exactly one difficulty metric (easy, medium, hard)"
+    print(f"Working on {path}...")
 
-    if is_easy: return "easy"
-    if is_medium: return "medium"
-    if is_hard: return "hard"
+    doc: Document = fitz.open(path)
+    last_page_no: int = 0
+    q_infos: list[SSQBInfo] = []
 
-    return
+    timer.start()
+    for page_num in range(len(doc)):
+        page = doc.load_page(page_num)
 
-meta_info = {
-    "parsed_at": str(dt.datetime.now()),
-    "source_pdf": pdf_path,
-    "difficulty": difficulty,
-}
+        text = page.get_text()
+        assert isinstance(text, str)
 
-doc = fitz.open(pdf_path)
-q_infos: list[SSQBInfo] = []
-start = time.time()
+        id: int = text.find("ID: ")
+        labels: int = text.find("Assessment")
 
-for page_num in range(len(doc)):
-    page = doc.load_page(page_num)
+        q_id_pat = r"ID: ([0-9a-f]*)"
+        matches = re.findall(q_id_pat, text[id:labels])
+        if len(matches) != 1:
+            # This probably means that one question takes up multiple pages
+            # print(f"No ID found in page {page_num + 1}")
+            continue
+        else:
+            last_page_no = page_num
 
-    text = page.get_text()
+        if page_num - last_page_no >= 1:
+            page_num_str = ""
+            for num in range(last_page_no, page_num+1):
+                page_num_str += str(num + 1)
+        else:
+            page_num_str = str(page_num + 1)
 
-    id: int = text.find("ID: ")
-    labels: int = text.find("Assessment")
+        q_id = matches[0]
 
-    q_id_pat = r"ID: ([0-9a-f]*)"
-    matches = re.findall(q_id_pat, text[id:labels])
-    assert len(matches) == 1
-    q_id = matches[0]
+        label_infos = ' '.join(text[labels:].split())
+        label_info_pat = r"Assessment (\w*) Test ([\w\s]*) Domain ([\w\s]*) Skill ([\w\s]*) D"
+        matches = re.findall(label_info_pat, label_infos)
+        for mat in matches:
+            assessment, test, domain, skill = mat
+            assert assessment == "SAT"
+            q_infos.append(SSQBInfo(
+                q_id,
+                domain,
+                skill,
+                page_num_str
+            ))
 
-    # print("------\n" + text[id:])
+    timer.stop(f"Completed parsing '{path}'")
 
-    label_infos = ' '.join(text[labels:].split())
+    timer.start()
+    q_dicts = []
+    data: dict = {
+        "ID": [],
+        "Pages": [],
+        "Domain": [],
+        "Skill": [],
+    }
+    for info in q_infos:
+        data["ID"].append(info.q_id)
+        data["Pages"].append(info.page_nos)
+        data["Domain"].append(info.domain)
+        data["Skill"].append(info.skill)
 
-    label_info_pat = r"Assessment (\w*) Test (\w*) Domain (\w*) Skill ([\w\s]*) D"
-    matches = re.findall(label_info_pat, label_infos)
-    for mat in matches:
-        assessment, test, domain, skill = mat
-        assert assessment == "SAT"
-        assert difficulty in ["easy", "medium", "hard"]
-        q_infos.append(SSQBInfo(
-            q_id,
-            assessment,
-            test,
-            domain,
-            skill,
-            difficulty,
-            page_num
-        ))
+    df: pd.DataFrame = pd.DataFrame(data)
+    output_path = f"{output_path_name.lower()}.{format}"
+    if format == "csv":
+        df.to_csv(output_path, index=False)
+    elif format == "json":
+        df.to_json(output_path, index=False)
+    else:
+        print(f"Failed to export: Unknown format ('{format}')")
+        print("Exporting to csv...")
+        df.to_csv(output_path, index=False)
 
-diff_ms = (time.time() - start) * 1000
-print(f"Completed parsing in {diff_ms} ms")
+    timer.stop(f"Completed exporting '{path}'\n-------------")
 
-start = time.time()
 
-q_dicts = []
-for q_i in q_infos:
-    q_dicts.append(asdict(q_i))
+meta_info_list: list[dict] = []
+for pdf_path, info in pdf_paths.items():
+    subject = info["subject"]
+    difficulty = info["difficulty"]
+    assert subject in ["Math", "R&W"], f"Subject('{subject}') must be either Math or R&W."
+    assert difficulty in ["easy", "medium", "hard"], (
+        f"Difficulty('{difficulty}') must be easy, medium, or hard"
+    )
 
-with open(f"{difficulty}-search-info.json", "w") as f:
-    json.dump({
-        "meta_info": meta_info,
-        "questions": q_dicts
-    }, f, indent=4)
+    meta_info_list.append({
+        "parsed_at": str(dt.datetime.now()),
+        "source_pdf": pdf_path,
+        "subject": subject,
+        "difficulty": difficulty,
+        "excluded": info["excluded"],
+    })
 
-diff_ms = (time.time() - start) * 1000
-print(f"Completed exporting in {diff_ms} ms")
+    output_name = pdf_output_path_name(**info)
+    process_question_pdf(pdf_path, output_name, "csv")
+
+with open("meta_infos.json", "w") as f:
+    json.dump(meta_info_list, f, indent=4)
